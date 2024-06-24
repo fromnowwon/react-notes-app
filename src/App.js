f54f1e60f9733ab6c43123529858d52313983ff5
cw <fromnowwon@gmail.com>
2024-06-24 19:37:22 +0900

    feat: 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Modal from "./components/Modal";
+import Card from "./components/Card";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Modal />
+			<header>
+				<h1>Notes</h1>
+				<button>+</button>
+			</header>
+			<div className="cards-container">
+				<Card />
+			</div>
+		</div>
+	);
 }
 
 export default App;

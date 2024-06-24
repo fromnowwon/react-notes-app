741ebd67f32614b82df039b14245817a0866e599
cw <fromnowwon@gmail.com>
2024-06-24 19:51:25 +0900

    feat: 모달 노출 여부 관련 상태 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 import Modal from "./components/Modal";
 import Card from "./components/Card";
 
 function App() {
+	const [notes, setNotes] = useState([]);
+	const [showModal, setShewModal] = useState(false);
+
+	const handleShowModal = () => {
+		setShewModal(!showModal);
+	};
+
 	return (
 		<div className="App">
-			<Modal />
+			{showModal && <Modal handleShowModal={handleShowModal} />}
 			<header>
 				<h1>Notes</h1>
-				<button>+</button>
+				<button onClick={handleShowModal}>+</button>
 			</header>
 			<div className="cards-container">
 				<Card />

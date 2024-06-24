38ced4df5d2b29266051e846d1c799cc19284087
cw <fromnowwon@gmail.com>
2024-06-24 20:35:00 +0900

    feat: toggle menu 추가

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = ({ note }) => {
+	const [showMenu, setShowMenu] = useState(false);
+
+	const handleShowMenu = () => {
+		setShowMenu(!showMenu);
+	};
+
 	return (
 		<div className="card">
-			<p>{note.text}</p>
-			<p>{note.date}</p>
+			<div className="toggle-menu">
+				<button className="toggle-btn" onClick={handleShowMenu}>
+					⁝
+				</button>
+				{showMenu && (
+					<div className="toggle-menu-list">
+						<ul>
+							<li>수정</li>
+							<li>삭제</li>
+						</ul>
+					</div>
+				)}
+			</div>
+			<p className="main-text">{note.text}</p>
+			<p className="date">{note.date}</p>
 		</div>
 	);
 };
